5337e7f10d8331b6a061a589aafb38ffaea5f0ef
Meagan-Kingren <drkingren@gmail.com>
2026-04-07 03:38:30 -0500

    fix website

--- R/add_exp_time.R
+++ R/add_exp_time.R
@@ -1,3 +1,20 @@
+#' Add experimental time information
+#'
+#' Use this function to add experiment and minute number based on your newly calculated hours
+#'
+#' @param dataset dataframe
+#' @param exp_hour_var exp.hour
+#'
+#' @returns dataframe with additional columns for exp.day and exp.min
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' add_exp_time(df1)
+#' add_exp_time(df1, exp_hour_var = "exp.hour")
+#' }
+#'
+#'
 add_exp_time <- function(dataset,
                          exp_hour_var = "exp.hour") {   # defaults to "exp.hour" column name
 
